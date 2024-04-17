c9f818bae08b963f3d8ac185cced063d1dd099ed
Ivan Kutsko <Wanr36@users.noreply.github.com>
2024-04-17 17:09:08 +0300

    Update VariableAppIconPlugin.swift

--- ios/Classes/VariableAppIconPlugin.swift
+++ ios/Classes/VariableAppIconPlugin.swift
@@ -23,32 +23,47 @@ public class VariableAppIconPlugin: NSObject, FlutterPlugin {
   //   }
   // }
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let arguments = call.arguments as? [String: Any] else { return }
-
-    switch call.method {
-    case "changeAppIcon":
-        let defaultIcon = arguments["defaultiOS"] as! String
-        let currentIcon = arguments["iosIcon"] as! String
-        let iconName = defaultIcon == currentIcon ? nil : currentIcon
-
-        UIApplication.shared.setAlternateIconName(iconName) { (error) in
-            if let error = error {
-                // Обработка ошибки
-                result(FlutterError(code: "IconChangeFailed", message: "Не удалось изменить иконку: \(error.localizedDescription)", details: nil))
-            } else {
-                // Показываем UIAlertController с информацией о смене иконки
-                if let viewController = UIApplication.shared.delegate?.window??.rootViewController {
-                    let alert = UIAlertController(title: "Смена иконки", message: "Иконка приложения была успешно изменена.", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    viewController.present(alert, animated: true, completion: nil)
-                }
-                result(nil)
+    UIApplication.shared.setAlternateIconName(iconName) { (error) in
+    if let error = error {
+        result(FlutterError(code: "IconChangeFailed", message: "Не удалось изменить иконку: \(error.localizedDescription)", details: nil))
+    } else {
+        // Показываем UIAlertController с информацией о смене иконки
+        DispatchQueue.main.async {
+            if let viewController = UIApplication.shared.delegate?.window??.rootViewController {
+                let alert = UIAlertController(title: "Смена иконки", message: "Иконка приложения была успешно изменена.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                viewController.present(alert, animated: true, completion: nil)
             }
         }
-
-    default:
-        result(FlutterMethodNotImplemented)
+        result(nil)
     }
+}
+    // guard let arguments = call.arguments as? [String: Any] else { return }
+
+    // switch call.method {
+    // case "changeAppIcon":
+    //     let defaultIcon = arguments["defaultiOS"] as! String
+    //     let currentIcon = arguments["iosIcon"] as! String
+    //     let iconName = defaultIcon == currentIcon ? nil : currentIcon
+
+    //     UIApplication.shared.setAlternateIconName(iconName) { (error) in
+    //         if let error = error {
+    //             // Обработка ошибки
+    //             result(FlutterError(code: "IconChangeFailed", message: "Не удалось изменить иконку: \(error.localizedDescription)", details: nil))
+    //         } else {
+    //             // Показываем UIAlertController с информацией о смене иконки
+    //             if let viewController = UIApplication.shared.delegate?.window??.rootViewController {
+    //                 let alert = UIAlertController(title: "Смена иконки", message: "Иконка приложения была успешно изменена.", preferredStyle: .alert)
+    //                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    //                 viewController.present(alert, animated: true, completion: nil)
+    //             }
+    //             result(nil)
+    //         }
+    //     }
+
+    // default:
+    //     result(FlutterMethodNotImplemented)
+    // }
 }
 
 }
